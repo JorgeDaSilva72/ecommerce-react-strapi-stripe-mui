1ae8ea08dd5d85d34ec77665613fa9b7c964b5bd
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-03-05 17:18:26 +0100

    Creation du composant ShoppingList

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,9 +1,11 @@
 import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
 
 function Home() {
   return (
     <div className="home">
       <MainCarousel />
+      <ShoppingList />
     </div>
   );
 }
